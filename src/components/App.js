b35696e5ddafb6f1e0c180cf9938464cc8e7d6cf
Sriram <43503475+ScionOfIksvaku@users.noreply.github.com>
2020-04-27 08:14:39 -0400

    Final Commit

---
+++ src/components/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import youtube from "../apis/youtube";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("Iskcon");
+  }
+
+  onTermSubmit = async (term) => {
+    const KEY = "AIzaSyC7cSAQ_06J8PEHef40YIn3GgLEZeMdZBw";
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: KEY,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({
+      selectedVideo: video,
+    });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
